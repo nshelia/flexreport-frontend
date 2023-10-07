edd8fefb522abd60868c65c9a8b270cae2646021
nshelia <nshprimary@gmail.com>
2023-10-07 20:28:01 +0400

    chore: add main deps

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
+import { QueryClientProvider } from 'react-query';
+import { Toaster } from 'react-hot-toast';
+import { queryClient } from './api/queryClient';
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
-      <Router />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider theme={theme}>
+        <Router />
+        <Toaster />
+      </MantineProvider>
+    </QueryClientProvider>
   );
 }
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,30 @@
-import { Welcome } from '../components/Welcome/Welcome';
+import { Container, Box } from '@mantine/core';
+import { useState } from 'react';
+import Header from '../components/Header';
+import DatabaseConnect from '../components/DatabaseConnect';
+import GenerateQuery from '../components/GenerateQuery';
+import ConfirmationQuery from '../components/ConfirmationQuery';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 
 export function HomePage() {
+  const [sessionId, setSessionId] = useState('');
+  const [activeQuery, setActiveQuery] = useState('');
+
+  const isQueryGenerated = !!activeQuery;
+
   return (
-    <>
-      <Welcome />
+    <Container size="xs" mt={10} pt="2%">
+      <Header />
+      <Box mt={10}>
+        <DatabaseConnect onConnect={setSessionId} />
+        <Box mt={10}>
+          <GenerateQuery sessionId={sessionId} onQueryId={setActiveQuery} />
+          {isQueryGenerated && (
+            <ConfirmationQuery queryId={activeQuery} queryPreview={activeQuery} />
+          )}
+        </Box>
+      </Box>
       <ColorSchemeToggle />
-    </>
+    </Container>
   );
 }
