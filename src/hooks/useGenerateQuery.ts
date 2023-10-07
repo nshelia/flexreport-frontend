d4d092a9aecae1c377b7601dc160323304eae063
nshelia <nshprimary@gmail.com>
2023-10-07 20:26:48 +0400

    chore: add query generator hook

---
+++ src/hooks/useGenerateQuery.ts
@@ -0,0 +1,28 @@
+import { useMutation } from 'react-query';
+import { toast } from 'react-hot-toast';
+import api from '../api';
+
+function useGenerateQuery() {
+  const { data, error, isLoading, mutate } = useMutation(
+    ({ sessionId, prompt }: { sessionId: string; prompt: string }) =>
+      api.generateQuery(sessionId, prompt),
+    {
+      onError: () => {
+        toast.error('Query generation failed');
+      },
+      onSuccess: () => {
+        toast.success('Generated');
+      },
+    }
+  );
+
+  return {
+    isLoading,
+    error,
+    sessionId: data?.sessionId,
+    queryId: data,
+    generate: mutate,
+  };
+}
+
+export default useGenerateQuery;
